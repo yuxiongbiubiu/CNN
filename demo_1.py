569c30da49a05f5b14def5c432b9895c0bc6a925
yuxiongxinxin <18yxiong1@stu.edu.cn>
2019-02-28 17:17:19 +0800

    CNN回归demo

---
+++ demo_1.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 12 12:15:17 2019
+by YuXiong
+
+"""
+
+from skimage import io,transform
+import os 
+import numpy as np
+import tensorflow as tf
+import time
+
+path='train_data/'
+
+model_path='model/model.ckpt'
+
+w=100
+h=400
+c=3
+
+#读取图片
+def read_img(path):
+    cate=os.listdir(path)
+    imgs = []
+    labels = []
+    for i in range(len(cate)):
+        img = io.imread(path+cate[i])
+        img=transform.resize(img,(w,h))
+        name_split = cate[i].split('_')
+        label = float(name_split[0])
+        imgs.append(img)
+        labels.append(label)
+    return np.asarray(imgs,np.float32),np.asarray(labels,np.float32)
+data,label=read_img(path)
+label = np.reshape(label,[len(label),1])
+
+#打乱顺序
+num_example=data.shape[0]
+arr=np.arange(num_example)
+np.random.shuffle(arr)
+data=data[arr]
+label=label[arr]
+
+
+#将所有数据分为训练集和验证集
+ratio=0.8
+s=np.int(num_example*ratio)
+x_train=data[:s]
+y_train=label[:s]
+x_val=data[s:]
+y_val=label[s:]
+
+#-----------------构建网络----------------------
+#占位符
+x=tf.placeholder(tf.float32,shape=[None,w,h,c],name='x')
+y_=tf.placeholder(tf.float32,shape=[None,1],name='y_')
+
+def inference(input_tensor, train, regularizer):
+    with tf.variable_scope('layer1-conv1'):
+        conv1_weights = tf.get_variable("weight",[5,20,3,32],initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv1_biases = tf.get_variable("bias", [32], initializer=tf.constant_initializer(0.0))
+        conv1 = tf.nn.conv2d(input_tensor, conv1_weights, strides=[1, 1, 1, 1], padding='SAME')
+        relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
+
+    with tf.name_scope("layer2-pool1"):
+        pool1 = tf.nn.max_pool(relu1, ksize = [1,2,2,1],strides=[1,2,2,1],padding="VALID")
+
+    with tf.variable_scope("layer3-conv2"):
+        conv2_weights = tf.get_variable("weight",[5,20,32,64],initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv2_biases = tf.get_variable("bias", [64], initializer=tf.constant_initializer(0.0))
+        conv2 = tf.nn.conv2d(pool1, conv2_weights, strides=[1, 1, 1, 1], padding='SAME')
+        relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))
+
+    with tf.name_scope("layer4-pool2"):
+        pool2 = tf.nn.max_pool(relu2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    with tf.variable_scope("layer5-conv3"):
+        conv3_weights = tf.get_variable("weight",[3,12,64,128],initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv3_biases = tf.get_variable("bias", [128], initializer=tf.constant_initializer(0.0))
+        conv3 = tf.nn.conv2d(pool2, conv3_weights, strides=[1, 1, 1, 1], padding='SAME')
+        relu3 = tf.nn.relu(tf.nn.bias_add(conv3, conv3_biases))
+
+    with tf.name_scope("layer6-pool3"):
+        pool3 = tf.nn.max_pool(relu3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    with tf.variable_scope("layer7-conv4"):
+        conv4_weights = tf.get_variable("weight",[3,12,128,128],initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv4_biases = tf.get_variable("bias", [128], initializer=tf.constant_initializer(0.0))
+        conv4 = tf.nn.conv2d(pool3, conv4_weights, strides=[1, 1, 1, 1], padding='SAME')
+        relu4 = tf.nn.relu(tf.nn.bias_add(conv4, conv4_biases))
+
+    with tf.name_scope("layer8-pool4"):
+        pool4 = tf.nn.max_pool(relu4, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+        nodes = 6*25*128
+        reshaped = tf.reshape(pool4,[-1,nodes])
+
+    with tf.variable_scope('layer9-fc1'):
+        fc1_weights = tf.get_variable("weight", [nodes, 1024],
+                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
+        if regularizer != None: tf.add_to_collection('losses', regularizer(fc1_weights))
+        fc1_biases = tf.get_variable("bias", [1024], initializer=tf.constant_initializer(0.1))
+
+        fc1 = tf.nn.relu(tf.matmul(reshaped, fc1_weights) + fc1_biases)
+        if train: fc1 = tf.nn.dropout(fc1, 0.5)
+
+    with tf.variable_scope('layer10-fc2'):
+        fc2_weights = tf.get_variable("weight", [1024, 512],
+                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
+        if regularizer != None: tf.add_to_collection('losses', regularizer(fc2_weights))
+        fc2_biases = tf.get_variable("bias", [512], initializer=tf.constant_initializer(0.1))
+
+        fc2 = tf.nn.relu(tf.matmul(fc1, fc2_weights) + fc2_biases)
+        if train: fc2 = tf.nn.dropout(fc2, 0.5)
+
+    with tf.variable_scope('layer11-fc3'):
+        fc3_weights = tf.get_variable("weight", [512, 1],
+                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
+        if regularizer != None: tf.add_to_collection('losses', regularizer(fc3_weights))
+        fc3_biases = tf.get_variable("bias", [1], initializer=tf.constant_initializer(0.1))
+        logit = tf.matmul(fc2, fc3_weights) + fc3_biases
+
+    return logit
+
+#---------------------------网络结束---------------------------
+regularizer = tf.contrib.layers.l2_regularizer(0.0001)
+logits = inference(x,False,regularizer)
+
+#(小处理)将logits乘以1赋值给logits_eval，定义name，方便在后续调用模型时通过tensor名字调用输出tensor
+b = tf.constant(value=1,dtype=tf.float32)
+logits_eval = tf.multiply(logits,b,name='logits_eval') 
+
+loss=tf.losses.mean_squared_error(logits_eval,y_)
+train_op=tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+#correct_prediction = tf.equal(tf.cast(tf.argmax(logits,1),tf.int32), y_)    
+#acc= tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+#定义一个函数，按批次取数据
+def minibatches(inputs=None, targets=None, batch_size=None, shuffle=False):
+    assert len(inputs) == len(targets)
+    if shuffle:
+        indices = np.arange(len(inputs))
+        np.random.shuffle(indices)
+    for start_idx in range(0, len(inputs) - batch_size + 1, batch_size):
+        if shuffle:
+            excerpt = indices[start_idx:start_idx + batch_size]
+        else:
+            excerpt = slice(start_idx, start_idx + batch_size)
+        yield inputs[excerpt], targets[excerpt]
+
+
+#训练和测试数据，可将n_epoch设置更大一些
+
+train_loss_plot = [0.0]
+train_acc_plot = [0.0]
+
+n_epoch=40                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+batch_size=10
+saver=tf.train.Saver()
+sess=tf.Session()  
+sess.run(tf.global_variables_initializer())
+
+for epoch in range(n_epoch):
+    start_time = time.time()
+
+    #training
+    train_loss, train_acc, n_batch = 0, 0, 0
+    for x_train_a, y_train_a in minibatches(x_train, y_train, batch_size, shuffle=True):
+        _,err=sess.run([train_op,loss], feed_dict={x: x_train_a, y_: y_train_a})
+        train_loss_plot.append(err)
+#        train_acc_plot.append(ac)
+        train_loss += err;  n_batch += 1
+    print("   train loss: %f" % (np.sum(train_loss)/ n_batch))
+#    print("   train acc: %f" % (np.sum(train_acc)/ n_batch))
+
+    #validation
+    val_loss, val_acc, n_batch = 0, 0, 0
+    for x_val_a, y_val_a in minibatches(x_val, y_val, batch_size, shuffle=False):
+        err = sess.run(loss, feed_dict={x: x_val_a, y_: y_val_a})
+        val_loss += err;  n_batch += 1
+    print("   validation loss: %f" % (np.sum(val_loss)/ n_batch))
+#    print("   validation acc: %f" % (np.sum(val_acc)/ n_batch))
+saver.save(sess,model_path)
+sess.close()
